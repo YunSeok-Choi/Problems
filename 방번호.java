897ac2300b6dbb65a811fa042976909a15460c9f
Yunseok-Choi <dbstjr9898@naver.com>
2022-02-20 14:55:22 +0900

    1475

---
+++ 방번호.java
@@ -0,0 +1,31 @@
+package implement;
+
+import java.util.*;
+
+public class 방번호 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int sum = 0;
+		int[] number = new int[9];
+		String a = sc.next();
+		String[] b = new String[a.length()];
+
+		b = a.split("");
+
+		for (int i = 0; i < a.length(); i++) {
+			if (b[i].equals("9"))
+				number[6]++;
+			else
+				number[Integer.parseInt(b[i])]++;
+		}
+		number[6] /= 2;
+
+		for (int i = 1; i < 9; i++) {
+			if (number[i] > sum)
+				sum = number[i];
+		}
+		System.out.println(sum);
+	}
+
+}
